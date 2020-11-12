4082ab9131e916d128a121e22778ff32350f0201
Francisco Rios <fcrios145@hotmail.com>
2020-11-11 21:59:04 -0600

    styles

--- frontend/components/browser/Home.js
+++ frontend/components/browser/Home.js
@@ -2,19 +2,7 @@ import React from 'react';
 import Layout from "./Layout";
 import {NavLink} from "react-router-dom";
 import { connect } from 'react-redux'
-
-const imageStyle = {
-    width: '100%', 
-    height: 'auto', 
-    maxWidth: '500px'
-}
-
-const imageStyleNone = {
-    width: '100%', 
-    height: 'auto', 
-    maxWidth: '500px',
-    display: 'none'
-}
+import Slider from '../browser/Slider'
 
 class Home extends React.Component {
     componentDidMount() {
@@ -44,42 +32,7 @@ class Home extends React.Component {
                         </div>
                     </div>
                     <div className='row not-mobile'>
-                        <div style={{display: 'flex', alignItems: 'flex-end', flexDirection: 'column'}}>
-                            <div style={{position: 'relative'}}>
-                                <img style={imageStyle} src='/static/images/slider-1.png' />
-                                <img style={imageStyleNone} src='https://source.unsplash.com/VFGEhLznjPU' />
-                                <div style={{
-                                    position: 'absolute',
-                                    left: '0',
-                                    top: '0',
-                                    width: '100%',
-                                    height: '100%',
-                                    display: 'flex',
-                                    justifyContent: 'center',
-                                    backgroundColor: 'rgba(51, 51, 51, 0.5)',
-                                    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'
-                                    }}>
-                                    <div style={{
-                                        display: 'flex', 
-                                        flexDirection: 'column', 
-                                        alignItems: 'center', 
-                                        justifyContent: 'flex-end',
-                                        }}>
-                                        <button style={{
-                                            backgroundColor: '#2196F3',
-                                            fontFamily: 'Montserrat',
-                                            border: '0',
-                                            fontSize: '1.5em',
-                                            color: '#E7F4FE',
-                                            fontWeight: '700',
-                                            padding: '1em 3em',
-                                            marginBottom: '1em',
-                                            borderRadius: '0.15em' 
-                                            }}>Ir al Menú</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <Slider/>
                     </div>
                 </div>
             </Layout>
--- frontend/components/browser/Plate.js
+++ frontend/components/browser/Plate.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import Layout from "./Layout";
 
+const liMenuStyle = {
+    color: '#053861',
+    fontSize: '1.2em',
+    marginBottom: '0.7em'
+};
+
+const spanTitle = {
+    fontSize: '1.5em',
+    color: '#2196F3'
+}
+
 export default class Plate extends React.Component {
 
     componentDidMount() {
@@ -24,7 +35,7 @@ export default class Plate extends React.Component {
         console.log(plate);
         return (
             <Layout {...this.props}>
-                <div className="plate">
+                <div className="plate mobile-only">
                     {plate &&
                     <React.Fragment>
                         <img style={{width: '100%', height: 'auto'}} className='plate-image' src="/static/images/ceviche.png" alt="ceviche" />
@@ -43,6 +54,60 @@ export default class Plate extends React.Component {
 
                     </React.Fragment>
                     }
+                </div>
+                <div className="not-mobile">
+                    <div className="rows">
+                        <div className="row" style={{display: 'flex', justifyContent: 'space-between', marginRight: '5em'}}>
+                            <div>
+                                <span style={spanTitle}>Aguachile</span>
+                                <ul style={{marginTop: '1.5em'}}>
+                                    <li style={liMenuStyle}>Mar verde</li>
+                                    <li style={liMenuStyle}>Mar Rojo</li>
+                                    <li style={liMenuStyle}>Mar Morado</li>
+                                </ul>
+                            </div>
+                            <div>
+                                <span style={spanTitle}>Aguachile</span>
+                                <ul style={{marginTop: '1.5em'}}>
+                                    <li style={liMenuStyle}>Mar verde</li>
+                                    <li style={liMenuStyle}>Mar Rojo</li>
+                                    <li style={liMenuStyle}>Mar Morado</li>
+                                    <li style={liMenuStyle}>Mar Morado</li>
+                                </ul>
+                            </div>
+                            <div>
+                                <span style={spanTitle}>Aguachile</span>
+                                <ul style={{marginTop: '1.5em'}}>
+                                    <li style={liMenuStyle}>Mar verde</li>
+                                    <li style={liMenuStyle}>Mar Rojo</li>
+                                    <li style={liMenuStyle}>Mar Morado</li>
+                                    <li style={liMenuStyle}>Mar Morado</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="row" style={{display: 'flex', justifyContent: 'center'}}>
+                            <div style={{display: 'flex', flexDirection: 'column'}}>
+                                <h1 style={{
+                                    fontWeight: '900',
+                                    fontSize: '2em',
+                                    color: '#2196F3',
+                                    textAlign: 'center'
+                                }}>{plate.name}</h1>
+                                <p style={{
+                                    fontSize: '1.5em',
+                                    textAlign: 'center',
+                                    color: '#053861',
+                                    margin: '1.5em 0'
+                                }}>
+                                    Camarón crudo acompañado con pepino tomate y cebolla moradaen cuadro, chile serrano y cilantro finamente picado coronado con aguacate
+                                </p>
+                                <img style={{width: '100%', height: 'auto', maxWidth: '500px', alignSelf: 'center', marginBottom: '1em'}} src="/static/images/slider-1.png" alt=""/>
+                            </div>
+
+                        </div>
+
+
+                    </div>
                 </div>
             </Layout>
         );
